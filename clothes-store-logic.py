5a9b7fa0b7e4cd192b9353e247ae1ce74c4ff720
tallyson <tallyson.eu@outlook.com>
2024-06-01 13:46:22 -0300

    feat: cria mock para arquivo principal

--- clothes-store-logic.py
+++ clothes-store-logic.py
@@ -7,26 +7,32 @@ class Store:
 
     def buy(self, quantity: int) -> float:
         total: float = 0
+        print(f'Voce comprou {quantity} itens na loja {self.name}')
         if quantity == 1:
             total = self.price
-            print(f'nao ha desconto nessa modalidade')
+            print(f'Nao ha desconto nessa modalidade')
 
         elif quantity == 2:
             total = self.price + (self.price * 0.7)
-            print(f'o desconto nessa modalidade e de 30% para a segunda camisa')
+            print(f'O desconto nessa modalidade e de 30% para a segunda camisa')
 
         elif quantity >= 3 and quantity <= 6:
             total = self.price * 2 + (self.price * (quantity - 2) * .5)
-            print(f'o desconto nessa modalidade e de 50% para todas camisas a mais que duas')
+            print(f'O desconto nessa modalidade e de 50% para todas camisas a mais que duas')
 
         elif quantity > 6:
             total = self.price * quantity * .5
-            print(f'o desconto nessa modalidade e de 50%')
+            print(f'O desconto nessa modalidade e de 50%')
 
+        print(f'O valor da compra foi de: {round(total, 2)}\n')
         return round(total, 2)
 
 
-hearing = Store('Hearing', 100000, 129.87)
+if __name__ == '__main__':
+    # create stores
+    hearing = Store('hearing', 100000, 129.99)
+    americans = Store('americans', 1000000, 49.97)
 
-print(f'o preco individual de cada peca de roupa e de: {hearing.price}')
-print(f'valor final: {hearing.buy(8)}')
+    # simulate a buy
+    hearing.buy(2)
+    americans.buy(1)
