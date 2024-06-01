0ceaab07d5c17a91d71a6e9693861c08aa1b60f4
tallyson <tallyson.eu@outlook.com>
2024-06-01 11:55:29 -0300

    feat: cria module da aplicacao

---
+++ module.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+def connect()->None:
+    print("Connecting...")
+    sleep(4)
+    print("Connection has been successfully implemented!")
+
+if __name__ == '__main__':
+    connect()
